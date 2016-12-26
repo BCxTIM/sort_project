21131c7ca5257187f6f0b8675f982d32039336c3
Timofei Moiseev <tmoiseev@endava.com>
2016-12-26 12:10:41 +0200

    add matrix

---
+++ src/Task2/Task12.java
@@ -0,0 +1,40 @@
+package Task2;
+
+import java.util.Random;
+
+/**
+ * Created by tmoiseev on 12/26/2016.
+ */
+public class Task12 {
+    private static int n = 3;
+    /*private static int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9},
+    };*/
+    private static int[][] matrix = new int[n][n];
+    private static Random random = new Random();
+
+    protected static void generateMatrix() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = random.nextInt(10);
+            }
+        }
+    }
+
+    private static void showMatrix() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(" " + matrix[i][j]);
+
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        generateMatrix();
+        showMatrix();
+    }
+}
