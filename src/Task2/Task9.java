ed073db7509c231a710f5a3f9d2a6ddd417a8d5f
Timofei Moiseev <tmoiseev@endava.com>
2016-12-28 10:38:21 +0200

    add task 9

--- src/Task2/Task9.java
+++ src/Task2/Task9.java
@@ -3,5 +3,33 @@ package Task2;
 /**
  * Created by tmoiseev on 12/20/2016.
  */
-public class Task9 {
+public class Task9 { //оптимизация - устранить поовторяющиеся числа
+
+    private static int n = 10000;
+
+    private static int sum(int i) {
+        int res = 0;
+        for (int j = 1; j <= i / 2; j++) {
+            if (i % j == 0) {
+                res += j;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("All friendly number pairs, smaller than " + n + " is: ");
+        for (int i = 1; i <= n; i++) {
+            if (sum(sum(i)) == i) {
+                System.out.println(i + " and " + sum(i));
+            }
+        }
+    }
 }
+
+
+//TODO оптимизация
+/*if(i != sum(i)) {
+        System.out.println(i + " and " + sum(i));
+}*/
