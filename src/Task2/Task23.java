8ccf2b40eb7ff7eccf03121cb99b90fb81362111
Timofei Moiseev <tmoiseev@endava.com>
2016-12-28 18:11:37 +0200

    fix array

--- src/Task2/Task23.java
+++ src/Task2/Task23.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
  * Created by tmoiseev on 12/28/2016.
  */
 public class Task23 {
-    private static int[] array = {2, 5, 3, 4, 2, 2, 2, 4, 8, 2, 5, 3, 3, 7};
+    private static int[] array = {2, 5, 3, 4, 2, 2, 2, 4, 8, 2, 5, 3, 3, 3, 7};
     // 2 2 2 2 2 3 3 3 7
 
 
@@ -19,7 +19,7 @@ public class Task23 {
 
         int pos = 0;
 
-       for (int i = pos; i < array.length; i ++) {
+       for (int i = 0; i < array.length; i ++) {
            for (int j = pos; j < array.length; j ++) {
                 if(array[j] == array[i]) {
                     System.out.print(" " + array[i]);
