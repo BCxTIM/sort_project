7c81ac404486e7d3c363086df66438bf4866da13
Timofei Moiseev <tmoiseev@endava.com>
2016-12-29 17:23:47 +0200

    tests

--- src/Task2/Task16.java
+++ src/Task2/Task16.java
@@ -4,5 +4,20 @@ package Task2;
  * Created by tmoiseev on 12/27/2016.
  */
 public class Task16 {
+    private static int n = 10;
+    private static int i = 1;
 
+    public static void main(String[] args) {
+        int moves = 0;
+
+        while (n != i) {
+            if(n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = n - 1;
+            }
+            moves ++;
+        }
+        System.out.println(moves);
+    }
 }
---
+++ src/Task2/Task20.java
@@ -0,0 +1,7 @@
+package Task2;
+
+/**
+ * Created by tmoiseev on 12/29/2016.
+ */
+public class Task20 {
+}
