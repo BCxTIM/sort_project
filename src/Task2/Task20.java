29f4ccfa32d4eb06cb653016d912e37d3c294099
Timofei Moiseev <tmoiseev@endava.com>
2016-12-29 17:52:14 +0200

    fix

--- src/Task2/Task20.java
+++ src/Task2/Task20.java
@@ -1,7 +1,25 @@
 package Task2;
 
+import java.util.Arrays;
+
 /**
  * Created by tmoiseev on 12/29/2016.
  */
 public class Task20 {
+
+    private static int[] array = {1, 3, 4, 6, 4, 6, 8, 9};
+
+    public static void main(String[] args) {
+
+
+
+        for (int i = 0; i < array.length - 1; i ++) {
+            int val = array[i];
+            if(array[i + 1] - 1 != val + 1) {
+                System.out.print(" " + array[i]);
+            }
+        }
+
+    }
+
 }
