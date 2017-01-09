05915da267c0a08b14734e5e46b9e65d5b9663ba
Timofei Moiseev <tmoiseev@endava.com>
2017-01-09 10:41:21 +0200

    add adjacment

--- src/Task2/Task20.java
+++ src/Task2/Task20.java
@@ -1,25 +1,29 @@
 package Task2;
 
-import java.util.Arrays;
 
 /**
  * Created by tmoiseev on 12/29/2016.
  */
 public class Task20 {
 
-    private static int[] array = {1, 3, 4, 6, 4, 6, 8, 9};
+    private static int[] array = {1, 3, 4, 6, 4, 6, 8, 12};
 
+
+    //Not optimized
     public static void main(String[] args) {
 
-
-
-        for (int i = 0; i < array.length - 1; i ++) {
-            int val = array[i];
-            if(array[i + 1] - 1 != val + 1) {
-                System.out.print(" " + array[i]);
-            }
+        if(array[0] + 1 != array[1] ) {
+            System.out.print(" " + array[0]);
+        }
+        for (int i = 1; i < array.length - 1; i ++) {
+           if(array[i] + 1 != array[i + 1]) {
+               System.out.print(" " + array[i]);
+           }
         }
 
+        if (array[array.length - 1] != array[array.length - 2] + 1) {
+            System.out.print(" " + array[array.length - 1]);
+        }
     }
 
 }
