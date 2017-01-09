9825aa5449a5930f8c0f88f93df9aedd5265dac7
Timofei Moiseev <tmoiseev@endava.com>
2017-01-09 11:24:56 +0200

    fix

--- src/Task2/Task20.java
+++ src/Task2/Task20.java
@@ -1,20 +1,34 @@
 package Task2;
 
 
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  * Created by tmoiseev on 12/29/2016.
  */
 public class Task20 {
 
-    private static int[] array = {1, 3, 4, 6, 4, 6, 7, 12};
+    private static int n = 10;
+    private static Random random = new Random();
+    private static int[] array = new int[n];
 
+    private static void generateArray() {
+        for (int i = 0; i < n; i ++) {
+            array[i] = random.nextInt(10);
+        }
+    }
 
     //Not optimized
     public static void main(String[] args) {
 
+        generateArray();
+        System.out.println(Arrays.toString(array));
+
         if(array[0] + 1 != array[1] ) {
             System.out.print(" " + array[0]);
         }
+
         for (int i = 1; i < array.length - 1; i ++) {
            if(array[i] != array[i + 1] - 1 && array[i] != array[i - 1] + 1) {
                System.out.print(" " + array[i]);
@@ -26,14 +40,5 @@ public class Task20 {
         }
     }
 
-    /*public static void main(String[] args) {
-
-        for (int i = 0; i < array.length - 1; i++) {
-            if(array[i] != array[i + 1] - 1) {
-                System.out.print(" " + array[i]);
-            }
-        }
-
-    }*/
 
 }
