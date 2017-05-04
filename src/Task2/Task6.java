dd3aa7d2cae27222ca263cceb4ab06b05fdabaae
Timofei Moiseev <tmoiseev@endava.com>
2017-05-04 16:25:02 +0300

    fix

--- src/Task2/Task6.java
+++ src/Task2/Task6.java
@@ -1,52 +1,27 @@
 package Task2;
 
-import java.util.Scanner;
 
 /**
  * Created by tmoiseev on 12/12/2016.
  */
 public class Task6 {
 
-//    private static int n = 400;
-//    private static double pi = Math.PI;
-//
-//    //формула числа пи TODO
-//
-//    public static void main(String[] args) {
-//        showPi();
-//    }
-//
-//    private static void showPi() {
-//        String formattedDouble = String.format("%."+ n + "f", pi);
-//        System.out.println(formattedDouble);
-//    }
 
     public static void main(String[] args) {
-        Double t;// точность
-        Double p; //значение пи
-        Double n; // число членов ряда
-        Double chr; // значение члена ряда
-
-        chr = 1.0D;
-        n = 1.0D;
-        p = 0.0D;
-        System.out.print("Задайте точность вычисления ПИ -> ");
-        Scanner scanner = new Scanner(System.in);
-        t = scanner.nextDouble();
-        System.out.println(t);
-        while (t < Math.abs(chr)) {
-            p += chr;
-
-            //chr = (float) (Math.pow(-1, (1 + n))) * (1 / (2 * n - 1));
-            chr = ((Math.pow(-1,  n)) * (1 / ((2 * n) + 1)));
-            n++;
+        int inCount = 0;
+        int count = 2000000;
+        double x;
+        double y;
+        double result;
+        for (int i = 0; i < count; i++) {
+            x = (Math.random() *2) - 1;
+            y = (Math.random() *2) - 1;
+            if(Math.pow(x, 2) + Math.pow(y, 2) <=1 ) {
+                inCount ++;
+            }
         }
-        p = p * 4;
 
-        System.out.printf("Значение числа ПИ с точностью %1.6f равно %1.6f\n", t, p);
-
-        System.out.println("Количество членов ряда " + n);
-        System.out.printf(" %3.6f\n ", chr);
-
+        result = (double)4*inCount/count;
+        System.out.println(result);
     }
 }
