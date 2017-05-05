91f43b3795b12bd64f1ef1e17b0df7ebef7dc8bd
Timofei Moiseev <tmoiseev@endava.com>
2017-05-06 01:54:24 +0300

    figure too

--- src/Task2/Figure.java
+++ src/Task2/Figure.java
@@ -15,8 +15,8 @@ public class Figure {
         points = new ArrayList<>();
     }
 
-    public void add(int x, int y) {
-        points.add(new Point(x, y));
+    public void add(Point point) {
+        points.add(point);
     }
 
 
@@ -28,6 +28,11 @@ public class Figure {
         for (Point p : points)
             System.out.println("(" + p.x + "," + p.y + ")");
     }
+
+    public void remove(int index) {
+        points.remove(index);
+    }
+
 
     public int length() {
         return points.size();
@@ -69,35 +74,67 @@ public class Figure {
         return intersect_count % 2 == 1;
     }
 
-    public void test(Figure a) {
+    public void findPerimetr(Figure a) {
+        Figure b = contur(a);
+
+        distance(b);
+        System.out.println(getPerimiter(b));
+    }
+
+    public Figure contur(Figure a) {
         Figure b = new Figure();
 
         for (int i = 0; i <= 2; i++) {
-            b.add(a.get(i).x, a.get(i).y);
+            b.add(a.get(i));
         }
-
 
         for (int i = 3; i < a.length(); i++) {
             if (!b.isInside(a.get(i).x, a.get(i).y)) {
-                b.add(a.get(i).x, a.get(i).y);
+                b.add(a.get(i));
             }
         }
-
-
-        b.print();
+        return b;
     }
 
     public static void main(String[] args) {
         Figure a = new Figure();
-        a.add(0, 5);
-        a.add(0, 0);
-        a.add(5, 5);
-        a.add(2, 7);
-        a.add(2, 6);
+        a.add(new Point(0, 5));
+        a.add(new Point(0, 0));
+        a.add(new Point(5, 5));
+        a.add(new Point(2, 6));
+        a.add(new Point(2, 7));
 
-
-        a.test(a);
+        a.findPerimetr(a);
     }
+
+
+    private static int[] distance(Figure figure) {
+        int[] length = new int[figure.length()];
+
+        for (int  i = 0; i < figure.length(); i ++ ) {
+
+
+            if(i == figure.length() - 1) {
+                length[i] = (int) figure.get(i).distance(figure.get(0));
+            } else {
+                length[i] = (int) figure.get(i).distance(figure.get(i+1));
+            }
+
+        }
+        return length;
+
+    }
+
+    private static int getPerimiter(Figure b) {
+        int[] length = distance(b);
+        int perimiter = 0;
+        for (int i = 0; i < length.length; i ++) {
+            System.out.println(length[i]);
+            perimiter += length[i];
+        }
+        return perimiter;
+    }
+
 
 
 }
--- src/Task2/Task22.java
+++ src/Task2/Task22.java
@@ -1,89 +1,89 @@
-package Task2;
-
-
-import java.awt.*;
-import java.math.BigDecimal;
-
-/**
- * Created by tmoiseev on 3/9/2017.
- */
-public class Task22 {
-
-
-    public static void main(String[] args) {
-        Point a = new Point(1, 1);
-        Point b = new Point(1, 5);
-        Point c = new Point(4, 5);
-        Point d = new Point(4, 1);
-        Point m = new Point(2, 2);
+//package Task2;
 //
-
-
-
-        Point[] figure = {a, b, c, d, m};
-//        contur(figure);
-        distance(figure);
-
-    }
-
-
-
-
-    private static void contur1(Point[] figure) {
-        int[] x = new int[figure.length];
-        int[] y = new int[figure.length];
-
-        int n = figure.length;
-
-        for(int i = 0; i < figure.length; i ++) {
-            x[i] = (int)figure[i].getX();
-            y[i] = (int)figure[i].getY();
-        }
-
-//        Polygon polygon = new Polygon(x, y, n);
-
-    }
-
-    private static void distance(Point[] figure) {
-        double[] length = new double[figure.length];
-
-        for (int  i = 0; i < figure.length; i ++ ) {
-
-
-            if(i == figure.length - 1) {
-                length[i] = roundDownScale(figure[i].distance(figure[0]));
-            } else {
-                length[i] = roundDownScale(figure[i].distance(figure[i + 1]));
-            }
-
-            System.out.println(length[i]);
-
-        }
-
-        System.out.println(getPerimiter(length));
-
-    }
-
-    private static double getPerimiter(double[] length) {
-        double perimiter = 0;
-        for (int i = 0; i < length.length; i ++) {
-            perimiter += length[i];
-        }
-        return perimiter;
-    }
-
-    public static double roundDownScale(double aValue)
-    {
-        BigDecimal decimal = new BigDecimal(aValue);
-        decimal = decimal.setScale(1,BigDecimal.ROUND_DOWN);
-        double result = decimal.doubleValue();
-        return result;
-    }
-
-
-
-
-
-
-
-}
+//
+//import java.awt.*;
+//import java.math.BigDecimal;
+//
+///**
+// * Created by tmoiseev on 3/9/2017.
+// */
+//public class Task22 {
+//
+//
+//    public static void main(String[] args) {
+//        Point a = new Point(1, 1);
+//        Point b = new Point(1, 5);
+//        Point c = new Point(4, 5);
+//        Point d = new Point(4, 1);
+//        Point m = new Point(2, 2);
+////
+//
+//
+//
+//        Point[] figure = {a, b, c, d, m};
+////        contur(figure);
+//        distance(figure);
+//
+//    }
+//
+//
+//
+//
+//    private static void contur1(Point[] figure) {
+//        int[] x = new int[figure.length];
+//        int[] y = new int[figure.length];
+//
+//        int n = figure.length;
+//
+//        for(int i = 0; i < figure.length; i ++) {
+//            x[i] = (int)figure[i].getX();
+//            y[i] = (int)figure[i].getY();
+//        }
+//
+////        Polygon polygon = new Polygon(x, y, n);
+//
+//    }
+//
+//    private static void distance(Point[] figure) {
+//        double[] length = new double[figure.length];
+//
+//        for (int  i = 0; i < figure.length; i ++ ) {
+//
+//
+//            if(i == figure.length - 1) {
+//                length[i] = roundDownScale(figure[i].distance(figure[0]));
+//            } else {
+//                length[i] = roundDownScale(figure[i].distance(figure[i + 1]));
+//            }
+//
+//            System.out.println(length[i]);
+//
+//        }
+//
+//        System.out.println(getPerimiter(length));
+//
+//    }
+//
+//    private static double getPerimiter(double[] length) {
+//        double perimiter = 0;
+//        for (int i = 0; i < length.length; i ++) {
+//            perimiter += length[i];
+//        }
+//        return perimiter;
+//    }
+//
+//    public static double roundDownScale(double aValue)
+//    {
+//        BigDecimal decimal = new BigDecimal(aValue);
+//        decimal = decimal.setScale(1,BigDecimal.ROUND_DOWN);
+//        double result = decimal.doubleValue();
+//        return result;
+//    }
+//
+//
+//
+//
+//
+//
+//
+//}
